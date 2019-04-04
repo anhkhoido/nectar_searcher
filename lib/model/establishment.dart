7e97934cebec2fd75290e81fef4b20bbe00c073b
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-03 23:22:09 -0400

    First commit! Woohoo!

---
+++ lib/model/establishment.dart
@@ -0,0 +1,33 @@
+import 'dart:core';
+
+class Establishment {
+  final String name;
+  final String address;
+  final String city;
+  final String province;
+  final String postalCode;
+  final String intersection;
+  final String subwayStation;
+  final String sunday;
+  final String monday;
+  final String tuesday;
+  final String wednesday;
+  final String thursday;
+  final String friday;
+  final String saturday;
+
+  Establishment(this.name,
+                this.address,
+                this.city,
+                this.province,
+                this.postalCode,
+                this.intersection,
+                this.subwayStation,
+                this.sunday,
+                this.monday,
+                this.tuesday,
+                this.wednesday,
+                this.thursday,
+                this.friday,
+                this.saturday);
+}
