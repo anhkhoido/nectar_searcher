5e23a8111bcdc8a1ad9a954aab5a29af1afdb9ff
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-04 08:39:37 -0400

    Changed main menu with images.

--- lib/view/main_menu.dart
+++ lib/view/main_menu.dart
@@ -1,11 +1,42 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:nectar_searcher/view/list_establishments.dart';
 
 class MainMenu extends StatelessWidget {
 
   final String title;
 
   MainMenu(this.title);
+
+  Widget buttonForMenuOfBars(BuildContext context) {
+    return RaisedButton(
+            child: Image.asset('assets/bar_button.png'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ListEstablishments("Bars"))
+              );
+            }
+    );
+  }
+
+  Widget buttonForBrewpubs(BuildContext context) {
+    return RaisedButton(
+      child: Image.asset('assets/brewpubs_button.png'),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ListEstablishments("Brewpubs")));
+      },
+    );
+  }
+
+  Widget buttonForMenuOfPubs(BuildContext context) {
+    return RaisedButton(
+      child: Image.asset('assets/pubs_button.png'),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ListEstablishments("Pubs")));
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +50,7 @@ class MainMenu extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Main menu'),
+            buttonForMenuOfBars(context),
           ],
         ),
       ),
