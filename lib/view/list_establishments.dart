b60743b2d10eb15867c942cdc699168afcefa16f
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-04 16:14:50 -0400

    Tested listing of bars.

--- lib/view/list_establishments.dart
+++ lib/view/list_establishments.dart
@@ -1,11 +1,72 @@
 import 'package:flutter/material.dart';
+import '../model/establishment.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ListEstablishments extends StatelessWidget {
-
-  final String title;
-  
+class ListEstablishments extends StatefulWidget
+{
+  String title;
   ListEstablishments(this.title);
-  
+
+  @override
+  State createState() => _ListEstablishments(this.title);
+}
+
+
+class _ListEstablishments extends State<ListEstablishments> {
+
+  String title;
+  List<Establishment> establishments;
+
+  _ListEstablishments(String title) {
+    this.title = title;
+    establishments = List<Establishment>();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    try {
+      this.getEstablishments();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<List<Establishment>> getEstablishments() async {
+    var link = "http://anhkhoido.com/nectar_searcher/" + title.toLowerCase() + ".json";
+    var response = await http.get(Uri.encodeFull(link), headers: {"Content-Type": "application/json"});
+    setState(() {
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        print(link);
+        var decodedResponse = jsonDecode(response.body);
+        var arrayOfEstablishments = decodedResponse[title.toLowerCase()] as List;
+        establishments = arrayOfEstablishments.map<Establishment>((json) => Establishment.fromJson(json)).toList();
+      }
+    });
+    print(establishments.length);
+    return establishments;
+  }
+
+  Widget listViewWidget(List<Establishment> establishment) {
+    print(this.establishments);
+    return Container(
+      child: ListView.builder(
+          itemCount: establishment.length,
+          padding: EdgeInsets.all(3.0),
+          itemBuilder: (context, position) {
+            return Card(
+              child: ListTile(
+                title: Text(establishment[position].name),
+                subtitle: Text(establishment[position].address),
+              ),
+            );
+          }
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,11 +75,9 @@ class ListEstablishments extends StatelessWidget {
         title: Text(title),
         backgroundColor: Colors.black,
       ),
-      body: ListView(
-        children: <Widget>[
-          Text("Bars to choose"),
-        ],
-      ),
+      body: (establishments != null) ?
+            listViewWidget(establishments) :
+            Center(child: CircularProgressIndicator())
     );
   }
 }
