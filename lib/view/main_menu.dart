bcb395dabdd359c1b7ee728a896d191cc9289a43
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-04 00:00:31 -0400

    Addition: images of splash screen for Android.

---
+++ lib/view/main_menu.dart
@@ -0,0 +1,17 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+}
