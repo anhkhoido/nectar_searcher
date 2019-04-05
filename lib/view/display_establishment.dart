9f75b588181612f34b41b12e236554675ba284a7
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-05 09:22:24 -0400

    First work on display_establishment.dart.

--- lib/view/display_establishment.dart
+++ lib/view/display_establishment.dart
@@ -7,26 +7,66 @@ class DisplayEstablishment extends StatelessWidget {
 
   DisplayEstablishment(this.establishment);
 
-  Widget columnWithDetailsAboutEstablishment(BuildContext context) {
-    return Column(
+  Widget listViewWithDetails(BuildContext context) {
+    return ListView(
       children: <Widget>[
-        
+        Text(
+          this.establishment.name,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22.0,
+          ),
+        ),
+        SizedBox(height: 5.0,),
+        Text(
+          this.establishment.address,
+          style: TextStyle(
+            fontSize: 17.0
+          ),
+        ),
+        SizedBox(height: 25.0,),
+        Text(
+          "Nearest major intersection",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22.0,
+            background: Paint()..color = Colors.green[700],
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          this.establishment.intersection,
+          style: TextStyle(
+            fontSize: 17.0,
+          ),
+        ),
+        SizedBox(height: 25.0,),
+        Text(
+          "Opening hours"
+        ),
+        SizedBox(height: 25.0,),
+        Text(
+          "Public transportation"
+        )
       ],
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text(this.establishment.name),
+        title: Text("Details"),
       ),
-      body: Container(
-        margin: EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 2.0),
-        child: columnWithDetailsAboutEstablishment(context),
-      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+          child: listViewWithDetails(context),
+        ),
+      )
     );
   }
 }
