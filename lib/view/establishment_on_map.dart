dfa152eba4f4de9d6cda9d3d3f122a3082721e03
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-05 19:52:03 -0400

    Created view/establishment_on_map.dart.

---
+++ lib/view/establishment_on_map.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class EstablishmentOnMap extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
+
+class _EstablishmentOnMap extends StatefulWidget {
+  @override
+  State createState() => _EstablishmentOnMapState();
+}
+
+class _EstablishmentOnMapState extends State<_EstablishmentOnMap>
+{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+    );
+  }
+}
