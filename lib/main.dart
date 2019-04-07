724a5ae2a81d3453469b02aae2409860967b6b8d
Anh Khoi Do <anhkhoido@gmail.com>
2019-04-06 20:10:34 -0400

    Removed debug banner.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Nectar Searcher',
       home: MainMenu('Nectar Searcher'),
     );
